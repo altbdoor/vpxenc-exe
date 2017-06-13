65f3c858f3d09d4511f7b519a64466fdcee35879
altbdoor <lancersupraskyline@gmail.com>
2017-06-13 18:22:47 +0800

    attempt to copy dll

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 
 # https://groups.google.com/a/webmproject.org/forum/#!topic/codec-devel/4tdpkYQ8xdA
 # https://github.com/jb-alvarado/media-autobuild_suite/blob/4957e2c4cdb946ba001b12add62b9ac8580987d4/media-autobuild_suite.bat
-LDFLAGS="$LDFLAGS -static-libgcc -static-libstdc++"
+# LDFLAGS="$LDFLAGS -static-libgcc -static-libstdc++"
 
 cd $APPVEYOR_BUILD_FOLDER/libvpx/
 ./configure --disable-docs --disable-unit-tests --disable-shared --enable-static --target=x86_64-win64-gcc
-
 make
