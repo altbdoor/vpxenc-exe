a3f89ad15e71bf344744cd4eff0e0ef3cead34f5
altbdoor <lancersupraskyline@gmail.com>
2017-06-14 12:35:24 +0800

    clean up the fluff

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# https://groups.google.com/a/webmproject.org/forum/#!topic/codec-devel/4tdpkYQ8xdA
-# https://github.com/jb-alvarado/media-autobuild_suite/blob/4957e2c4cdb946ba001b12add62b9ac8580987d4/media-autobuild_suite.bat
-
-mkdir /c/pacman-cache
 pacman -Sy --noconfirm --cache /c/pacman-cache make automake-wrapper perl yasm gcc
 
 cd $APPVEYOR_BUILD_FOLDER/libvpx/
 
-# LDFLAGS="$LDFLAGS -static-libgcc -static-libstdc++"
 # --disable-shared --enable-static
 ./configure --disable-docs --disable-unit-tests --target=x86_64-win64-gcc
 make
@@ -20,4 +15,5 @@ cp libvpx/vpxdec.exe binary/
 cp /usr/bin/msys-2.0.dll binary/
 cp /usr/bin/msys-gcc_s-seh-1.dll binary/
 cp /usr/bin/msys-stdc++-6.dll binary/
-7z a binary.zip binary
+
+7z a -tzip -mx9 binary.zip binary
