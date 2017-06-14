5a1f5489770ade61d6b73c28d49b8ad88f20bc05
altbdoor <lancersupraskyline@gmail.com>
2017-06-14 10:33:22 +0800

    build no static

--- build.sh
+++ build.sh
@@ -8,15 +8,16 @@ pacman -Sy --noconfirm --cache /c/pacman-cache make automake-wrapper perl yasm g
 
 cd $APPVEYOR_BUILD_FOLDER/libvpx/
 
-LDFLAGS="$LDFLAGS -static-libgcc -static-libstdc++"
-./configure --disable-docs --disable-unit-tests --disable-shared --enable-static --target=x86_64-win64-gcc
+# LDFLAGS="$LDFLAGS -static-libgcc -static-libstdc++"
+# --disable-shared --enable-static
+./configure --disable-docs --disable-unit-tests --target=x86_64-win64-gcc
 make
 
 cd $APPVEYOR_BUILD_FOLDER/
 mkdir binary
 cp libvpx/vpxenc.exe binary/
 cp libvpx/vpxdec.exe binary/
-# cp /cygdrive/c/cygwin-root/bin/cygwin1.dll binary/
-# cp /cygdrive/c/cygwin-root/bin/cygstdc++-6.dll binary/
-# cp /cygdrive/c/cygwin-root/bin/cyggcc_s-seh-1.dll binary/
+cp /usr/bin/msys-2.0.dll binary/
+cp /usr/bin/msys-gcc_s-seh-1.dll binary/
+cp /usr/bin/msys-stdc++-6.dll binary/
 7z a binary.zip binary
